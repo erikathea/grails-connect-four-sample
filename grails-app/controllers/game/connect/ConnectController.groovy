ec107230d8bb95abdb2e58de233fd6d724170cd0
erikathea <ethajes@gmail.com>
2015-04-05 21:41:33 +0800

    added default values to board dimension and player names

--- grails-app/controllers/game/connect/ConnectController.groovy
+++ grails-app/controllers/game/connect/ConnectController.groovy
@@ -1,14 +1,14 @@
 package game.connect
 
 class ConnectController {
-	def index() {
-		
-	}
-	
+	def index() {}
+
 	def play = {
-		int rows = Integer.parseInt(params.rows)
-		int cols = Integer.parseInt(params.cols)
-		[board:new Board(rows, cols), playerA:params.playerA, playerB:params.playerB]
+		int rows = params.row ? Integer.parseInt(params.rows) : 6
+		int cols = params.col ? Integer.parseInt(params.cols) : 7
+    def playerA = params.playerA ? params.playerA : "Player A"
+    def playerB = params.playerB ? params.playerB : "Player B"
+		[board:new Board(rows, cols), playerA: playerA, playerB: playerB]
 	}
-	
+
 }
